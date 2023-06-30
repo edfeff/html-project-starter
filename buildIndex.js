aed8f98e0c1cc8be02459fc0c40376ffe0e0fbc5
wpp <pengpeng.wang@gemii.cc>
2023-06-30 11:09:04 +0800

    feat: 构建index目录

---
+++ buildIndex.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+const dirs = fs.readdirSync(__dirname);
+
+//读取需要配置到html的目录
+const projectNames = [];
+dirs.forEach((name) => {
+  if (name.charAt(0) >= "A" && name.charAt(0) <= "Z") {
+    const filePath = path.join(__dirname, name);
+    if (fs.lstatSync(filePath).isDirectory()) {
+      projectNames.push(name);
+    }
+  }
+});
+//处理目录
+const context = {};
+
+const navList = projectNames.map((p) => {
+  return `<li><a href="#${p.replace(" ", "-")}">${p}</a></li>`;
+});
+context.NAV_LIST = navList.join("\n");
+
+const contentList = projectNames.map((p) => {
+  return `<iframe id="${p.replace(" ", "-")}" src="${p}/index.html"></iframe>`;
+});
+context.CONTENT = contentList.join("\n");
+
+let indexTemplate = fs.readFileSync("index-template.html", {
+  encoding: "utf-8",
+});
+
+Object.entries(context).forEach(([k, v]) => {
+  const key = `{{${k}}}`;
+  indexTemplate = indexTemplate.replace(key, v);
+});
+fs.writeFileSync("index.html", indexTemplate);
