8a0929aa655d92408174faa8a6dccc6e784994b8
wpp <pengpeng.wang@gemii.cc>
2023-06-30 16:34:52 +0800

    Scroll Animation debug

--- Scroll Animation/script.js
+++ Scroll Animation/script.js
@@ -1,21 +1,8 @@
 const boxes = document.querySelectorAll(".box");
 window.addEventListener("scroll", checkBoxes);
 
-// const div = document.createElement("div");
-// div.style.position = "fixed";
-// div.style.top = "0";
-// div.style.left = "0";
-// div.style.width = "100%";
-// div.style.zIndex = "100";
-// div.style.backgroundColor = "rgba(0,0,0,.1)";
-// div.style.height = (window.innerHeight / 5) * 4 + "px";
-// document.body.appendChild(div);
-
 function checkBoxes(e) {
   const toggleBottom = (window.innerHeight / 5) * 4;
-
-  // div.style.height = toggleBottom + "px";
-
   boxes.forEach((el) => {
     const top = el.getBoundingClientRect().top;
     if (top < toggleBottom) {
@@ -25,3 +12,21 @@ function checkBoxes(e) {
     }
   });
 }
+
+//DEBUG
+const div = document.createElement("div");
+div.style.position = "fixed";
+div.style.top = "0";
+div.style.left = "0";
+div.style.width = "100%";
+div.style.zIndex = "1";
+// div.style.backgroundColor = "rgba(0,0,0,.1)";
+div.style.borderBottom = "1px solid black";
+div.style.height = (window.innerHeight / 5) * 4 + "px";
+document.body.appendChild(div);
+div.style.display = "none";
+const debug = document.getElementById("debug");
+debug.addEventListener("click", () => {
+  div.style.display = div.style.display === "block" ? "none" : "block";
+});
+//end DEBUG
