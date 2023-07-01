b66b1ad31d5fe4ce307c18be544e915696e78ea3
wpp <396859442@qq.com>
2023-07-01 23:05:04 +0800

    Sound Board

---
+++ Sound Board/script.js
@@ -0,0 +1,23 @@
+const buttons = document.getElementById("buttons");
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
+
+  buttons.appendChild(btn);
+});
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
